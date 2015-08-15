65ed3813cb9b911dd0f9742c38fa81b68a41a958
Jorith van den Heuvel <jorith@gmail.com>
2015-08-15 14:35:13 +0200

    add confirmation message when leaving the page in changed state

--- global/js/angular.blikje.js
+++ global/js/angular.blikje.js
@@ -1,6 +1,7 @@
 var app = angular.module('blikje', []);
 
 app.controller('blikjeCtrl', function($scope) {
+	$scope.stateChanged = false;
 	$scope.totalAmount = 0;
 	$scope.blikjes = [
 		{'name': 'Cola', 'price': 0.7, 'amount': 0},
@@ -11,6 +12,12 @@ app.controller('blikjeCtrl', function($scope) {
 		{'name': '7-Up', 'price': 0.7, 'amount': 0},
 	];
 	
+	window.onbeforeunload = function (e) {
+		if ($scope.stateChanged) {
+			return "Ingevulde gegevens gaan verloren.";
+		}
+	};
+
 	$scope.plusOne = function(blikje) {
 		blikje.amount += 1;
 		updateTotal();
@@ -24,6 +31,7 @@ app.controller('blikjeCtrl', function($scope) {
 	};
 	
 	function updateTotal() {
+		$scope.stateChanged = true;
 		var totalAmount = 0;
 		angular.forEach($scope.blikjes, function(blikje) {
 		  totalAmount += (blikje.price * blikje.amount);
