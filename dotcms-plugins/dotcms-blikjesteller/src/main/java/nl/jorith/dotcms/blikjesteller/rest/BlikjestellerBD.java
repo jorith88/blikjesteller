5477bc8dd9f9064b192ec32e3378aadf2b6b39e8
Jorith van den Heuvel <jorith@isaac.nl>
2015-11-06 16:41:29 +0100

    add some code to prevent spam

--- dotcms-plugins/dotcms-blikjesteller/src/main/java/nl/jorith/dotcms/blikjesteller/rest/BlikjestellerBD.java
+++ dotcms-plugins/dotcms-blikjesteller/src/main/java/nl/jorith/dotcms/blikjesteller/rest/BlikjestellerBD.java
@@ -3,6 +3,8 @@ package nl.jorith.dotcms.blikjesteller.rest;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import nl.jorith.dotcms.blikjesteller.email.EmailFacade;
 import nl.jorith.dotcms.blikjesteller.rest.type.Blikje;
 import nl.jorith.dotcms.blikjesteller.util.ContentletQuery;
@@ -11,9 +13,12 @@ import com.dotcms.repackage.com.google.gson.Gson;
 import com.dotcms.repackage.javax.ws.rs.GET;
 import com.dotcms.repackage.javax.ws.rs.POST;
 import com.dotcms.repackage.javax.ws.rs.Path;
+import com.dotcms.repackage.javax.ws.rs.WebApplicationException;
+import com.dotcms.repackage.javax.ws.rs.core.Context;
+import com.dotcms.repackage.javax.ws.rs.core.Response.Status;
+import com.dotcms.repackage.org.apache.commons.lang.StringUtils;
 import com.dotcms.rest.WebResource;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.util.Logger;
 import com.google.gson.reflect.TypeToken;
 
 @Path("/blikjesteller")
@@ -44,11 +49,18 @@ public class BlikjestellerBD extends WebResource {
 	}
 
 	@POST @Path("send-order")
-	public void sendOrder(String blikjesJSON) {
-		List<Blikje> blikjes = gson.fromJson(blikjesJSON, new TypeToken<List<Blikje>>(){}.getType());
+	public void sendOrder(String blikjesJSON, @Context HttpServletRequest request) {
+		String userAgent = request.getHeader("User-Agent");
 
-		if (blikjes.size() > 0) {
-			emailFacade.sendBlikjesOrder(blikjes);
+		if (StringUtils.isNotBlank(userAgent) && !userAgent.contains("iPhone OS 3_2")  && !userAgent.contains("Apache-HttpClient")) {
+			List<Blikje> blikjes = gson.fromJson(blikjesJSON, new TypeToken<List<Blikje>>(){}.getType());
+
+			if (blikjes.size() > 0) {
+				emailFacade.sendBlikjesOrder(blikjes);
+			}
+		} else {
+			throw new WebApplicationException(Status.FORBIDDEN);
 		}
+
 	}
 }
