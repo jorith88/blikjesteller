7bbd98a6907ae7a4fc27f73ff91bb8f1f020d516
Jorith van den Heuvel <jorith@isaac.nl>
2015-12-10 17:24:44 +0100

    replace sendEmail method

--- dotcms-plugins/dotcms-blikjesteller/src/main/java/nl/jorith/dotcms/blikjesteller/facade/EmailFacade.java
+++ dotcms-plugins/dotcms-blikjesteller/src/main/java/nl/jorith/dotcms/blikjesteller/facade/EmailFacade.java
@@ -1,25 +1,22 @@
 package nl.jorith.dotcms.blikjesteller.facade;
 
 import java.text.NumberFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import nl.jorith.dotcms.blikjesteller.config.Configuration;
 import nl.jorith.dotcms.blikjesteller.rest.type.Blikje;
 
 import com.dotmarketing.util.Logger;
-import com.liferay.util.JNDIUtil;
 
 public class EmailFacade {
 
@@ -49,80 +46,36 @@ public class EmailFacade {
 
 		Logger.info(this, "Send blikjes order to " + emailTo + ". BCC to " + emailBcc);
 
-		sendEmail(emailTo, emailBcc, "blikje@jorith.nl", "Blikjesbestelling", mailBody.toString(), true);
+		sendMail(emailTo, emailBcc, "blikje@jorith.nl", "Blikjesbestelling", mailBody.toString());
 	}
 
-	private String sendEmail(String to, String bcc, String from, String subject,
-			String message, Boolean html) {
-		Session s = null;
-		try {
-			Context ctx = new InitialContext();
-			s = (Session) JNDIUtil.lookup(ctx, "mail/MailSession");
-		} catch (NamingException e1) {
-			Logger.error(this, e1.getMessage(), e1);
-		}
+	private void sendMail(String to, String bcc, String from, String subject, String message) {
+		Properties props = new Properties();
+        props.put("mail.smtp.host", "localhost");
+        props.put("mail.smtp.port", "25");
 
-		if (s == null) {
-			Logger.debug(this, "No Mail Session Available.");
-			return "";
-		}
-		String smtpServer = s.getProperty("mail.smtp.host");
+        Session mailSession = Session.getDefaultInstance(props);
+        Message msg = new MimeMessage(mailSession);
 
-		Properties props = System.getProperties();
-		props.put("mail.host", smtpServer);
-		props.setProperty("mail.transport.protocol", "smtp");
-		Session session = Session.getDefaultInstance(props, null);
+        InternetAddress fromAddress = null;
+        try {
+			fromAddress = new InternetAddress(from);
 
-		Message msg = new MimeMessage(session);
-		try {
-			msg.setFrom(new InternetAddress(from));
-			msg.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse(to, false));
-			msg.setRecipients(Message.RecipientType.BCC,
-					InternetAddress.parse(bcc, false));
-		} catch (Exception e) {
-			Logger.error(this, "Error Assigning To and From Addresses", e);
-			return "Invalid To and/or From Address: " + e.getMessage();
-		}
-		try {
+
+			msg.setFrom(fromAddress);
+
+			msg.addRecipient(RecipientType.TO, new InternetAddress(to));
+			msg.addRecipient(RecipientType.BCC, new InternetAddress(bcc));
+
 			msg.setSubject(subject);
-		} catch (Exception e) {
-			Logger.error(this, "Error Assigning Subject", e);
-			return "Invalid Subject: " + e.getMessage();
-		}
 
-		if (html.booleanValue())
-			try {
-				msg.setContent(message, "text/html");
-			} catch (Exception e) {
-				Logger.error(this, "Error Setting Content", e);
-				return "Invalid Message: " + e.getMessage();
-			}
-		else {
-			try {
-				msg.setText(message);
-			} catch (Exception e) {
-				Logger.error(this, "Error Assigning Text", e);
-				return "Invalid Message: " + e.getMessage();
-			}
+			msg.setContent(message, "text/html");
 
-		}
-
-		try {
-			msg.setHeader("X-Mailer", "DotCMSSimpleMailer");
-			msg.setSentDate(new Date());
-
-			Transport transport = session.getTransport();
-			transport.connect();
-			transport.sendMessage(msg,
-					msg.getRecipients(Message.RecipientType.TO));
-
-			transport.close();
-		} catch (Exception e) {
+			Transport.send(msg);
+		} catch (MessagingException e) {
 			Logger.error(this, "Error Sending Message", e);
-			return "Unable to Send Message: " + e.getMessage();
+			throw new RuntimeException(e);
 		}
 
-		return "";
 	}
 }
