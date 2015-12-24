a0b762cab93814e655ad8e4bad7ba2ff399931df
Jorith van den Heuvel <jorith@gmail.com>
2015-12-24 10:02:04 +0100

    add GsonProvider

--- src/main/java/nl/jorith/blikjesteller/bd/BlikjestellerBD.java
+++ src/main/java/nl/jorith/blikjesteller/bd/BlikjestellerBD.java
@@ -18,9 +18,6 @@ import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.lang.StringUtils;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import nl.jorith.blikjesteller.bd.type.Blikje;
 import nl.jorith.blikjesteller.config.Configuration;
 import nl.jorith.blikjesteller.facade.BlikjesFacade;
@@ -33,22 +30,20 @@ public class BlikjestellerBD {
 	private static final Logger LOGGER = Logger.getLogger(Configuration.class.getName());
 	private static final String TOKEN_KEY = "nl.jorith.blikjesteller.token";
 	
-	private static final Gson gson = new Gson();
 	private static final EmailFacade emailFacade = new EmailFacade();
 	private static final BlikjesFacade blikjesFacade = new BlikjesFacade();
 	
 	@GET @Path("/blikjes")
-	public String getBlikjes(@Context HttpServletRequest request) {
+	public List<Blikje> getBlikjes(@Context HttpServletRequest request) {
 		
 		// Set a token to prevent calls to /send-order only
 		request.getSession().setAttribute(TOKEN_KEY, UUID.randomUUID().toString());
 
-		return gson.toJson(blikjesFacade.getAllBlikjes());
+		return blikjesFacade.getAllBlikjes();
 	}
 
 	@POST @Path("send-order")
-	public void sendOrder(String orderJSON, @Context HttpServletRequest request) {
-		Map<String,Integer> order = gson.fromJson(orderJSON, new TypeToken<Map<String,Integer>>(){}.getType());
+	public void sendOrder(Map<String, Integer> order, @Context HttpServletRequest request) {
 		
 		long numberOfNotZeroBlikjes = order.values()
 									.stream()
