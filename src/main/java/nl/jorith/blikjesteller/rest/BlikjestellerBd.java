c5c3bb955cf314b54dcadcf2f61b72063df7fd05
Jorith van den Heuvel <jorith@isaac.nl>
2016-04-13 16:06:53 +0200

    remove request from REST endpoints

--- src/main/java/nl/jorith/blikjesteller/rest/BlikjestellerBd.java
+++ src/main/java/nl/jorith/blikjesteller/rest/BlikjestellerBd.java
@@ -6,13 +6,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -24,7 +22,7 @@ import nl.jorith.blikjesteller.rest.type.Blikje;
 @Path("/blikjesteller")
 @Produces(MediaType.APPLICATION_JSON)
 public class BlikjestellerBd {
-	
+
 	private static final Logger logger = Logger.getLogger(BlikjestellerBd.class.getName());
 
 	@Inject
@@ -32,13 +30,13 @@ public class BlikjestellerBd {
 
 	@GET
 	@Path("/blikjes")
-	public List<Blikje> getBlikjes(@Context HttpServletRequest request, @HeaderParam("Debug") boolean debugMode) {
+	public List<Blikje> getBlikjes(@HeaderParam("Debug") boolean debugMode) {
 		return blikjesFacade.getAllBlikjes();
 	}
 
 	@POST
 	@Path("send-order")
-	public Response sendOrder(Map<String, Integer> order, @Context HttpServletRequest request, @HeaderParam("Debug") boolean debugMode) {
+	public Response sendOrder(Map<String, Integer> order, @HeaderParam("Debug") boolean debugMode) {
 
 		try {
 			blikjesFacade.sendOrder(order, debugMode);
@@ -46,7 +44,7 @@ public class BlikjestellerBd {
 			logger.log(Level.WARNING, "Received an order without token...");
 			return Response.status(Status.FORBIDDEN).build();
 		}
-		
+
 		return Response.noContent().build();
 	}
 }
