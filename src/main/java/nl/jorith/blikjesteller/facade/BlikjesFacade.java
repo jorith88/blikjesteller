94505fbee90d8d9daf2a4888116f54d28e9bc4bf
Jorith van den Heuvel <jorith@isaac.nl>
2016-04-22 13:00:02 +0200

    add logging

--- src/main/java/nl/jorith/blikjesteller/facade/BlikjesFacade.java
+++ src/main/java/nl/jorith/blikjesteller/facade/BlikjesFacade.java
@@ -19,19 +19,21 @@ import nl.jorith.blikjesteller.rest.type.SendOrderAccessBean;
 @Stateless
 public class BlikjesFacade {
 	private static final Logger logger = Logger.getLogger(BlikjesFacade.class.getName());
-	
+
 	@Inject
 	private Configuration config;
-	
+
 	@Inject
 	private EmailFacade emailFacade;
-	
+
 	@Inject
 	private SendOrderAccessBean sendOrderAccess;
 
 	public List<Blikje> getAllBlikjes() {
 		sendOrderAccess.init();
-		
+
+		logger.info("Retrieve all blikjes");
+
 		List<Blikje> blikjes = config.getApplicationConfig().getBlikjes();
 
 		return blikjes.stream()
@@ -40,28 +42,29 @@ public class BlikjesFacade {
 	}
 
 	public void sendOrder(Map<String, Integer> order, boolean debugMode) throws SendOrderNotAllowedException {
-		
 		sendOrderAccess.checkAccess();
-		
+
+		logger.info("Send order!");
+
 		long numberOfNotZeroBlikjes = order.values().stream()
 											.filter(i -> i > 0)
 											.count();
-		
+
 		// We don't accept empty orders
 		if (numberOfNotZeroBlikjes == 0) {
 			logger.log(Level.WARNING, "Received an empty order...");
 			throw new WebApplicationException(Status.BAD_REQUEST);
 		}
-		
+
 		List<Blikje> orderedBlikjes = getOrderedBlikjes(order);
-		
+
 		emailFacade.sendBlikjesOrder(orderedBlikjes, debugMode);
 	}
-	
+
 	private List<Blikje> getOrderedBlikjes(Map<String,Integer> order) {
-		
+
 		List<Blikje> blikjes = config.getApplicationConfig().getBlikjes();
-		
+
 		return blikjes.stream()
 				.filter (blikje -> order.keySet().contains(blikje.getId())) // Only get blikjes that are ordered
 				.filter (blikje -> order.get(blikje.getId()) > 0) // Only get blikjes that have an amount > 0
